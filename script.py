f1b9205fa6c37a5c099fc8adcacb76b3a8b55ed4
Sourya Kakarla <thisisma08@gmail.com>
2015-01-27 14:12:38 +0530

    Added rBytes

--- script.py
+++ script.py
@@ -5,14 +5,18 @@ enq = []
 deq = []
 recv = []
 drop = []
+tBytes = 0
+rBytes = 0
 for line in f.readlines():
 	if("UdpHeader" in line):
 		if(line[0] == '+'):
 			enq.append(line.split(" "))
 		elif(line[0] == '-'):
 			deq.append(line.split(" "))
+			tBytes+=int((deq[-1][38])[6:-1])
 		elif(line[0] == 'r'):
 			recv.append(line.split(" "))
+			rBytes+=int((recv[-1][38])[6:-1])
 		elif(line[0] == 'd'):
 			drop.append(line.split(" "))
 		
@@ -32,7 +36,10 @@ delay=0
 for i in range(len(recv)):
 	delay+=float(recv[i][1])-float(deq[i][1])
 
+
 print "delaySum :",delay
+print "tBytes :",tBytes
+print "rBytes :",rBytes
 	
 
 
